99e36cc9a4910d7f9f3ec602fe2eb1416e7c9413
Lyoid <lodzomudiks@gmail.com>
2026-05-06 18:09:01 +0200

    added picking playlist logic

--- video_stats.py
+++ video_stats.py
@@ -10,6 +10,7 @@ load_dotenv(dotenv_path="./.env")
 
 CHANNEL_HANDLE = "MrBeast"
 API_KEY = os.getenv("API_KEY")
+MaxResults = 50
 
 def get_playlist_id():
 
@@ -33,6 +34,41 @@ def get_playlist_id():
         raise e
         
 
+
+def get_video_id(playlistID):
+
+    video_ids = []
+    pageToken = None
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={MaxResults}&playlistId={playlistID}&key={API_KEY}"
+
+    try:
+        while True:
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+        return video_ids
+            
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistID = get_playlist_id()
+    print(get_video_id(playlistID))
+
         
